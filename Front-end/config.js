c9fb1df00523ac0a34fe673eb0b4b92c49322885
Edrian Aldrin Mariñas <edrian.a.marinas@gmail.com>
2026-03-18 00:16:47 +0800

    Updated config

--- Front-end/config.js
+++ Front-end/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
   BACKEND_URL: window.location.hostname === "127.0.0.1" || window.location.hostname === "localhost"
     ? "http://127.0.0.1:8000"
-    : "https://edrian-personal-ai-backend.vercel.app/"
+    : "https://edrian-personal-ai-backend.vercel.app"
 };
